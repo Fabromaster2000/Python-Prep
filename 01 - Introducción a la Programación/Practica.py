860452759677c3859cd17c58ac4ef40fe468a249
Fabromaster2000 <faberalbi@gmail.com>
2022-10-18 07:44:22 -0300

    Data Science 1

--- 01 - Introducción a la Programación/Practica.py
+++ 01 - Introducción a la Programación/Practica.py
@@ -1,1 +1,15 @@
 print('Hola Mundo!222')
+my_var = "Protein Rox"
+print(my_var)
+
+
+contador = 0
+while contador<10:
+    print(contador)
+    contador = contador + 1
+    if contador>6:
+        break
+
+
+frutas = ['manzana','pera','mango']
+print (frutas)
